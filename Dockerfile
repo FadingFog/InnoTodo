d4858e512d714a7e19b65e099ca1008d6d1adca3
FadingFog <aleksey.guzlov@gmail.com>
2023-03-24 19:13:09 +0300

    Added alembic. Refactored nginx config. Fixed typo in config & .env.sample

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,5 @@ RUN poetry config virtualenvs.create false && \
 
 COPY . /app
 
+CMD ["alembic upgrade head"]
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/config.py
+++ app/config.py
@@ -2,12 +2,12 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@postgres:5432/postgres"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:postgres@db:5432/postgres"
 
-    PRIVATE_SECRET_KEY: str = 'Hdpl8E4HNSYjI4YcLF2TjqgEMFaeghratyEe6lbVRVs='
+    PRIVATE_SECRET_KEY: str = "Hdpl8E4HNSYjI4YcLF2TjqgEMFaeghratyEe6lbVRVs="
 
-    KAFKA_TOPIC: str = 'statistics'
-    KAFKA_BOOTSTRAP_SERVERS: str = 'kafka:9093'
+    KAFKA_TOPIC: str = "statistics"
+    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9093"
 
     class Config:
         env_file = ".env"
